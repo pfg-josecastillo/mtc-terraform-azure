0aaa14561b7dce4d2e03e3af2abca059db1d6524
pfg-josecastillo <jose.castillo@pfg-services.com>
2022-04-17 16:17:44 -1000

    added resource group, VNET and subnet

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "mtc-rg" {
+  name     = "mtc-resources"
+  location = "West US 2"
+  tags = {
+    "environment" = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "mtc-vn" {
+  name                = "mtc-network"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  location            = azurerm_resource_group.mtc-rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    "environment" = "dev"
+  }
+}
