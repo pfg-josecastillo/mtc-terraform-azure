8bd4ce9fe55c271048a4810dea57a7aa47bd6d43
pfg-josecastillo <jose.castillo@pfg-services.com>
2022-04-19 16:05:28 -1000

    added nsg

--- main.tf
+++ main.tf
@@ -38,15 +38,15 @@ resource "azurerm_subnet" "mtc-subnet" {
   address_prefixes     = ["10.123.1.0/24"]
 }
 
-# resource "azurerm_network_security_group" "mtc-sg" {
-#   name                = "mtc-sg"
-#   location            = azurerm_resource_group.mtc-rg.location
-#   resource_group_name = azurerm_resource_group.mtc-rg.name
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
 
-#   tags = {
-#     "environment" = "dev"
-#   }
-# }
+  tags = {
+    "environment" = "dev"
+  }
+}
 
 # resource "azurerm_network_security_rule" "mtc-dev-rule" {
 #   name                        = "mtc-dev-rule"
