dfd8953f27f49be2410b6be9aed3ce7e5ce72aec
pfg-josecastillo <jose.castillo@pfg-services.com>
2022-04-19 16:07:36 -1000

    added nsg rule

--- main.tf
+++ main.tf
@@ -48,19 +48,19 @@ resource "azurerm_network_security_group" "mtc-sg" {
   }
 }
 
-# resource "azurerm_network_security_rule" "mtc-dev-rule" {
-#   name                        = "mtc-dev-rule"
-#   priority                    = 100
-#   direction                   = "Inbound"
-#   access                      = "Allow"
-#   protocol                    = "*"
-#   source_port_range           = "*"
-#   destination_port_range      = "*"
-#   source_address_prefix       = "76.172.17.172/32"
-#   destination_address_prefix  = "*"
-#   resource_group_name         = azurerm_resource_group.mtc-rg.name
-#   network_security_group_name = azurerm_network_security_group.mtc-sg.name
-# }
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
+  name                        = "mtc-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "76.172.17.172/32"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
+}
 
 # resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
 #   subnet_id                 = azurerm_
